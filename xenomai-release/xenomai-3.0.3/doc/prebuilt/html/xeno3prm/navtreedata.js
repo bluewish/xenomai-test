b58a25d0db54471878c3832f4d1e1ed1422b762a
Lu Ken <ken.lu@intel.com>
2018-03-27 14:59:03 +0800

    add many xenomai releases

---
+++ xenomai-release/xenomai-3.0.3/doc/prebuilt/html/xeno3prm/navtreedata.js
@@ -0,0 +1,45 @@
+var NAVTREE =
+[
+  [ "Xenomai", "index.html", [
+    [ "API service tags", "api-tags.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"16550A__io_8h_source.html",
+"functions_g.html",
+"group__analogy__channel__range.html#ga9cb4bca9f87b9fb4a34d0db84323e32d",
+"group__cobalt__api__cond.html#ga68d1e544c6bbf3cedeee5455b14c642d",
+"group__rtdm.html#gadd2020d36782ebb916b065d6554f2631",
+"group__rtdm__device__register.html#ga4a225724822b506bb2ed036ef141e428",
+"group__rtdm__task.html#ga43315f778f88bdd7415504c6f6d0e0af",
+"kernel_2cobalt_2posix_2signal_8h_source.html",
+"structRT__QUEUE__INFO.html#afd5e97a2c5e83451dab949b6f43b9f1b",
+"structxnvfile__snapshot__ops.html#a930d19008b6053e42ef6919cee8a0d95"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
